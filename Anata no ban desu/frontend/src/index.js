f9fcef8d0ce575072ff9e3140661cfaf07081ba2
IzumiExarch <baijun0512@gmail.com>
2025-02-23 22:29:53 +0800

    maybe

---
+++ Anata no ban desu/frontend/src/index.js
@@ -0,0 +1,17 @@
+// 引入必要的模組
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // 如果有樣式檔案，這裡引入
+import App from './App'; // 引入主組件
+import reportWebVitals from './reportWebVitals'; // 可選，用來測量性能
+
+// 渲染 React 組件到 DOM
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // React 應該會將組件渲染到這個 DOM 元素中
+);
+
+// 如果你想測量性能，可以傳入 reportWebVitals
+reportWebVitals();
