c98e8a144571b1cce3d4c689bbd92ba673ff6a9b
IzumiExarch <baijun0512@gmail.com>
2025-02-24 23:37:30 +0800

    Update index.js

--- Anata no ban desu/frontend/src/index.js
+++ Anata no ban desu/frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // 假設有一個 App.js
-import reportWebVitals from './reportWebVitals'; // 如果有這個功能
 
 ReactDOM.render(
   <React.StrictMode>
